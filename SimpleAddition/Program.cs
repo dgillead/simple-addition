219b3c0554c4185b44610e46f86fc8aadce6ab9e
Darnell Gillead <dlgillead@gmail.com>
2017-09-12 14:44:16 -0500

    completed simple addition program

---
+++ SimpleAddition/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SimpleAddition
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the first number you would like to add: ");
+            int num1;
+
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second number you would like to add: ");
+            int num2;
+
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The sum of those numbers is: " + (num1 + num2));
+        }
+    }
+}
